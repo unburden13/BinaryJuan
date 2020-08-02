cf2617fe02451619c5e1fab3d0c989fdbd2458e8
Juan David González O <50647406+unburden13@users.noreply.github.com>
2020-08-01 19:10:31 -0500

    Root folder creation

---
+++ binary_juan/root/precache-manifest.fe408bc2a9a4c4e5934760031f66085a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bec66ab8ede3899d8ae4b1c579567f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e039248677878430a1e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c6ab763f005e3a2788e",
+    "url": "/static/css/main.54604199.chunk.css"
+  },
+  {
+    "revision": "3e039248677878430a1e",
+    "url": "/static/js/2.a7ecd3c6.chunk.js"
+  },
+  {
+    "revision": "5c5ef74666b63f7749ade2501e980c3a",
+    "url": "/static/js/2.a7ecd3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6ab763f005e3a2788e",
+    "url": "/static/js/main.23478965.chunk.js"
+  },
+  {
+    "revision": "51958ae404433fe5ef9f",
+    "url": "/static/js/runtime-main.d09db333.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
